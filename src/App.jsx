1ae42edf523d9ca42ad3178f2ef3254fbca57fd8
carolvanela7-del <carolvanela7@gmail.com>
2026-05-26 15:32:26 -0600

    Full responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1126,4 +1126,4 @@ export default function App() {
   if (role==="admin")  return <AdminView   envios={envios} setEnvios={setEnvios} userName={userName} onLogout={handleLogout}/>;
   if (role==="driver") return <DriverView  envios={envios} setEnvios={setEnvios} userName={userName} onLogout={handleLogout}/>;
   return null;
-} 
+}
